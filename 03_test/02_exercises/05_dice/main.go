adf61296967ef1c2e151d60cdd7a91e8336e070d
Gythoniel <31692630+Gythoniel@users.noreply.github.com>
2017-10-08 11:49:05 +0200

    added descriptions

--- 03_test/02_exercises/05_dice/main.go
+++ 03_test/02_exercises/05_dice/main.go
@@ -1,3 +1,14 @@
+/*
+Project Euler 205: Peter has nine four-sided (pyramidal) dice, each with faces numbered 1, 2, 3, 4.
+Colin has six six-sided (cubic) dice, each with faces numbered 1, 2, 3, 4, 5, 6.
+
+Peter and Colin roll their dice and compare totals: the highest total wins.
+The result is a draw if the totals are equal.
+
+What is the probability that Pyramidal Pete beats Cubic Colin?
+Give your answer rounded to seven decimal places in the form 0.abcdefg
+*/
+
 package main
 
 import (
@@ -8,6 +19,7 @@ import (
 // inspired by https://blog.dreamshire.com/project-euler-205-solution/
 func main() {
 
+	// p and c are the possibilities of reaching a certain score p(9-36) c(6-36)
 	p := []float64{0, 0, 0, 1, 9, 45, 165, 486, 1206, 2598, 4950, 8451, 13051, 18351, 23607,
 		27876, 30276, 30276, 27876, 23607, 18351, 13051, 8451, 4950, 2598, 1206, 486, 165, 45, 9, 1}
 	c := []float64{1, 6, 21, 56, 126, 252, 456, 756, 1161, 1666, 2247, 2856, 3431, 3906, 4221,
@@ -15,12 +27,19 @@ func main() {
 	var w float64
 	var t float64 = math.Pow(4, 9) * math.Pow(6, 6)
 
+	// range over c and multiply the amount of possibilities to arrive at that point in c
+	// by the amount of possibilities that P has a higher score than this point in c
+
 	for i := 0; i < len(c); i++ {
 		for j := i + 1; j < len(p); j++ {
 			w += p[j] * c[i]
 		}
 	}
-	fmt.Println("solution to euler 205: ", round(w/t, .5, 7))
+
+	//  possibilities that p wins (w) divided by the total
+	//  number of possibilities (t) results in P(win)
+
+	fmt.Print("solution to euler 205: ", round(w/t, .5, 7))
 }
 
 // found at https://gist.github.com/DavidVaini/10308388
@@ -37,14 +56,3 @@ func round(val float64, roundOn float64, places int) (newVal float64) {
 	newVal = round / pow
 	return
 }
-
-/*
-Project Euler 205: Peter has nine four-sided (pyramidal) dice, each with faces numbered 1, 2, 3, 4.
-Colin has six six-sided (cubic) dice, each with faces numbered 1, 2, 3, 4, 5, 6.
-
-Peter and Colin roll their dice and compare totals: the highest total wins.
-The result is a draw if the totals are equal.
-
-What is the probability that Pyramidal Pete beats Cubic Colin?
-Give your answer rounded to seven decimal places in the form 0.abcdefg
-*/
